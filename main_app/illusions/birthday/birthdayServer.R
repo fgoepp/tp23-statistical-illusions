3a5a17d3807c6969a35333aadf82685f1269492d
mqrek <116188885+mqrek@users.noreply.github.com>
2023-06-15 15:48:58 +0200

    added Ui and Server

--- main_app/illusions/birthday/birthdayServer.R
+++ main_app/illusions/birthday/birthdayServer.R
@@ -1,14 +1,16 @@
 birthdayServer <- function(id) {
   moduleServer(id, function(input, output, session) {
+    # updates category slider when a category is selected
     observeEvent(input$category_selector, {
       if (input$category_selector == "birthday in a year") {
         updateSliderInput(session, "c", value = 365, max = 400)
       } else if (input$category_selector == "favourite planet of our solarsystem") {
         updateSliderInput(session, "c", value = 8, max = 400)
       } else if (input$category_selector == "favorite element of the periodic table") {
-        updateSliderInput(session, "c", value = 118, max = 118)
+        updateSliderInput(session, "c", value = 118, max = 400)
       }
     })
+
 
     calculateProbability <- function(n, c) {
       probabilities <- sapply(1:n, function(i) {
@@ -23,9 +25,28 @@ birthdayServer <- function(id) {
       c <- input$c
       probabilities <- calculateProbability(input$n, c)
       data <- data.frame(n = n, p = probabilities)
-      plot <- barplot(data$p, names.arg = data$n, main = "Probability Distribution", xlab = "Number of people", ylab = "Probability in %")
+      plot <- barplot(data$p,
+        names.arg = data$n, main = "Probability Distribution", xlab = "Number of people", ylab = "Probability in %",
+        ylim = c(0, 100), border = "black"
+      )
+      # Add x-axis line
+      abline(h = 0, col = "black", lwd = 2)
+      # Real probability
       abline(h = (1 - prod(1 - (1:(input$n - 1)) / input$c)) * 100, col = "red", lwd = 2)
-      abline(h = 2, col = "green", lwd = 2)
+
+      # check whether guess is valid input
+      if (!is.null(input$guess_input)) {
+        guess <- as.numeric(input$guess_input)
+        if (!is.na(guess) && guess >= 0 && guess <= 100) {
+          abline(h = guess, col = "blue", lwd = 2)
+        } else {
+          output$guess_text <- renderText({
+            "Invalid guess input. Please enter a number between 0 and 100."
+          })
+        }
+      }
+      # legend
+      legend("topleft", legend = c("Real Probability", "Your Guess"), col = c("red", "blue"), lwd = 1)
       plot
     })
 
@@ -33,9 +54,11 @@ birthdayServer <- function(id) {
     output$probability_text <- renderText({
       paste0("Real probability: ", sprintf("%.4f", (1 - prod(1 - (1:(input$n - 1)) / input$c)) * 100), "%")
     })
+    output$guess_text <- renderText({
+      paste0("Your Guess: ", sprintf("%s", input$guess_input), "%")
+    })
 
-
-    # Explanation
+    # Explanation / Text / History etc
     output$text_description <- renderText({
       "history mistory etc"
     })
@@ -50,7 +73,10 @@ birthdayServer <- function(id) {
         c <- input$c
         probabilities <- calculateProbability(input$n, c)
         data <- data.frame(n = n, p = probabilities)
-        barplot(data$p, names.arg = data$n, main = "Probability Distribution", xlab = "Person", ylab = "Probability")
+        barplot(data$p,
+          names.arg = data$n, main = "Probability Distribution", xlab = "Person", ylab = "Probability",
+          ylim = c(0, 100), border = "black"
+        )
         dev.off()
       }
     )
--- main_app/illusions/birthday/birthdayUI.R
+++ main_app/illusions/birthday/birthdayUI.R
@@ -1,29 +1,57 @@
 birthdayUI <- function(id) {
   ns <- NS(id)
-  source("layout.R")
-  # sidebar Input
-  sideBarInput <- tagList(
-    sliderInput(ns("n"), "Number of people:", min = 1, max = 200, value = 70),
-    sliderInput(ns("c"), "Number of categories:", min = 1, max = 365, value = 365),
-    selectInput(
-      ns("category_selector"),
-      label = "Example category: ",
-      choices = c("birthday in a year", "favourite planet of our solarsystem", "favorite element of the periodic table"),
-      selected = "birthday in a year"
+
+  fluidPage(
+    # for coloring probabilty_text and guess_text
+    tags$head(
+      tags$style(HTML(".real-probability {color: red;}.your-guess {color: blue;}"))
     ),
-    downloadButton(ns("download_plot"), "Download Plot")
-  )
 
-  # mainPanel Output
-  mainPanelOutput <- tagList(
-    h4("Plot:"),
-    plotOutput(ns("probability_plot")),
-    textOutput(ns("probability_text")),
-    br(),
-    HTML("<h4>History</h4>"),
-    textOutput(ns("text_description"))
-  )
+    # Page 1: Introduction
+    conditionalPanel(
+      condition = "!input.page2_button",
+      div(
+        id = "page1",
+        style = "position: absolute; top: 25%; left: 50%; transform: translate(-50%, -50%); text-align: left;",
+        h3("What do you think is the probability that two random selected people of 70 in a room have the same day of birth in a year?"),
+        div(
+          numericInput(ns("guess_input"), "take your guess in percent", value = 0, min = 0, max = 100),
+          HTML("%"),
+          actionButton("page2_button", "Take Guess", )
+        )
+      )
+    ),
 
-  # load layout
-  layout(sideBarInput, mainPanelOutput)
+    # Page 2
+    conditionalPanel(
+      condition = "input.page2_button",
+      div(
+        id = "page2",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput(ns("n"), "Number of people:", min = 1, max = 200, value = 70),
+            sliderInput(ns("c"), "Number of categories:", min = 1, max = 365, value = 365),
+            selectInput(
+              ns("category_selector"),
+              label = "Example category: ",
+              choices = c("birthday in a year", "favourite planet of our solarsystem", "favorite element of the periodic table"),
+              selected = "birthday in a year"
+            ),
+            br(),
+            downloadButton(ns("download_plot"), "Download Plot"),
+          ),
+          mainPanel(
+            tagList(
+              plotOutput(ns("probability_plot")),
+              tags$span(textOutput(ns("probability_text")), class = "real-probability"),
+              tags$span(textOutput(ns("guess_text")), class = "your-guess"),
+              br(),
+              HTML("<h4>History</h4>"),
+              textOutput(ns("text_description"))
+            )
+          )
+        )
+      )
+    )
+  )
 }
