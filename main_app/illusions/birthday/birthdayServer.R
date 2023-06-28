d8383cb17ba510cd48dc5d4fe8bc343c050339e8
mqrek <116188885+mqrek@users.noreply.github.com>
2023-06-28 10:58:46 +0200

    changed name of page2_button to avoid conflicts

--- main_app/illusions/birthday/birthdayServer.R
+++ main_app/illusions/birthday/birthdayServer.R
@@ -11,15 +11,7 @@ birthdayServer <- function(id) {
       }
     })
 
-    calculateProbabilityBday2 <- function(n, c) {
-      probabilities <- sapply(1:n, function(i) {
-        p <- (1 - ((c - 1) / c)^i) * 100
-        p
-      })
-      probabilities
-    }
-
-    calculateProbabilityBday1 <- function(n, c) {
+    calculateProbability <- function(n, c) {
       probabilities <- sapply(1:n, function(i) {
         p <- (1 - prod(1 - (0:(i - 1)) / c)) * 100
         p
@@ -30,134 +22,62 @@ birthdayServer <- function(id) {
     output$probability_plot <- renderPlot({
       n <- 1:input$n
       c <- input$c
-      probabilities_bday1 <- calculateProbabilityBday1(input$n, c)
-      probabilities_bday2 <- calculateProbabilityBday2(input$n, c)
-
-
-      data <- data.frame(n = n, p1 = probabilities_bday1, p2 = probabilities_bday2)
-      # guesses data
-      first_guess_data <- data.frame(
-        x = rep(barplot(data$p1)[input$n], 2),
-        y = as.numeric(input$first_guess_input)
-      )
-      second_guess_data <- data.frame(
-        x = rep(barplot(data$p1)[input$n - 30], 2),
-        y = as.numeric(input$second_guess_input)
-      )
-      # answers data
-      first_answer_data <- data.frame(
-        x = rep(barplot(data$p1)[input$n], 2),
-        y = (1 - ((input$c - 1) / input$c)^(input$n)) * 100
-      )
-      second_answer_data <- data.frame(
-        x = rep(barplot(data$p1)[input$n - 30], 2),
-        y = (1 - prod(1 - (1:((input$n - 30) - 1)) / input$c)) * 100
-      )
-
-
-      plot <- barplot(data$p1,
-        names.arg = data$n,
-        main = "Probability Distribution",
-        col = "lightgrey",
-        xlab = "Number of people",
-        ylab = "Probability in %",
+      probabilities <- calculateProbability(input$n, c)
+      data <- data.frame(n = n, p = probabilities)
+      plot <- barplot(data$p,
+        names.arg = data$n, main = "Probability Distribution", xlab = "Number of people", ylab = "Probability in %",
         ylim = c(0, 100), border = "black"
       )
-      barplot(data$p2,
-        names.arg = data$n, add = TRUE, col = "darkgrey"
-      )
-
       # Add x-axis line
       abline(h = 0, col = "black", lwd = 2)
+      # Real probability
+      abline(h = (1 - prod(1 - (1:(input$n - 1)) / input$c)) * 100, col = "red", lwd = 2)
 
-
-
-      if (input$n == 70 && input$c == 365) {
-        # point for second answer
-        points(first_answer_data$x, first_answer_data$y, lwd = 3, col = "darkgreen", pch = 19)
-        points(second_answer_data$x, second_answer_data$y, lwd = 3, col = "darkblue", pch = 19)
-        # check whether first guess is valid input
-        if (!is.null(input$first_guess_input)) {
-          first_guess <- as.numeric(input$first_guess_input)
-          if (!is.na(first_guess) && first_guess >= 0 && first_guess <= 100) {
-            # point for first guess if valid
-            points(first_guess_data$x, first_guess_data$y, lwd = 3, col = "darkgreen", pch = 4)
-          } else {
-            output$first_guess_text <- renderText({
-              "Invalid guess input. Please enter a number between 0 and 100."
-            })
-          }
-        }
-
-        # check whether second guess is valid input
-        if (!is.null(input$second_guess_input)) {
-          second_guess <- as.numeric(input$second_guess_input)
-          if (!is.na(second_guess) && second_guess >= 0 && second_guess <= 100) {
-            # point for second guess if valid
-            points(second_guess_data$x, second_guess_data$y, lwd = 3, col = "darkblue", pch = 4)
-          } else {
-            output$second_guess_text <- renderText({
-              "Invalid guess input. Please enter a number between 0 and 100."
-            })
-          }
+      # check whether guess is valid input
+      if (!is.null(input$guess_input)) {
+        guess <- as.numeric(input$guess_input)
+        if (!is.na(guess) && guess >= 0 && guess <= 100) {
+          abline(h = guess, col = "blue", lwd = 2)
+        } else {
+          output$guess_text <- renderText({
+            "Invalid guess input. Please enter a number between 0 and 100."
+          })
         }
       }
       # legend
-      legend("topleft",
-        inset = 0.01,
-        pch = c(15, 15, 19, 4, 19, 4),
-        col = c("lightgrey", "darkgrey", "darkgreen", "darkgreen", "darkblue", "darkblue"),
-        legend = c(
-          "distribution first problem", "distribution second problem",
-          "probability question one", "first guess", "probability question two", "second guess"
-        ),
-        text.col = c("black", "black", "darkgreen", "darkgreen", "darkblue", "darkblue")
-      )
+      legend("topleft", legend = c("Real Probability", "Your Guess"), col = c("red", "blue"), lwd = 1)
       plot
     })
 
-
-    output$first_question_text <- renderText({
-      paste0(
-        "What do you think is the probability that one of n (here n = 70)
-        people present shares the same day of birth as you in a year?"
-      )
+    # real probability
+    output$probability_text <- renderText({
+      paste0("Real probability: ", sprintf("%.4f", (1 - prod(1 - (1:(input$n - 1)) / input$c)) * 100), "%")
     })
-    output$second_question_text <- renderText({
-      paste0(
-        "What do you think is the probability that two
-        or more people of n (here n = 40) share the same day of birth in a year?"
-      )
+    output$guess_text <- renderText({
+      paste0("Your Guess: ", sprintf("%s", input$guess_input), "%")
     })
-    output$first_guess_text <- renderText({
-      paste0(
-        "Your First Guess: ",
-        sprintf("%s", input$first_guess_input), "%"
-      )
-    })
-    output$first_answer_text <- renderText({
-      paste0(
-        "Propability: ",
-        sprintf("%.4f", (1 - ((input$c - 1) / input$c)^(input$n)) * 100), "%"
-      )
-    })
-    output$second_guess_text <- renderText({
-      paste0(
-        "Your Second Guess: ",
-        sprintf("%s", input$second_guess_input), "%"
-      )
-    })
-    output$second_answer_text <- renderText({
-      paste0(
-        "Propability: ",
-        sprintf("%.4f", (1 - prod(1 - (1:((input$n - 30) - 1)) / input$c)) * 100), "%"
-      )
-    })
-
 
     # Explanation / Text / History etc
     output$text_description <- renderText({
       "history mistory etc"
     })
+
+    output$download_plot <- downloadHandler(
+      filename = function() {
+        "plot.jpg"
+      },
+      content = function(file) {
+        jpeg(file)
+        n <- 1:input$n
+        c <- input$c
+        probabilities <- calculateProbability(input$n, c)
+        data <- data.frame(n = n, p = probabilities)
+        barplot(data$p,
+          names.arg = data$n, main = "Probability Distribution", xlab = "Person", ylab = "Probability",
+          ylim = c(0, 100), border = "black"
+        )
+        dev.off()
+      }
+    )
   })
 }
--- main_app/illusions/birthday/birthdayUI.R
+++ main_app/illusions/birthday/birthdayUI.R
@@ -2,50 +2,29 @@ birthdayUI <- function(id) {
   ns <- NS(id)
 
   fluidPage(
-    # for coloring probabilty_text and guess_text
+    # for coloring probability_text and guess_text
     tags$head(
-      tags$style(HTML(".first-answer {color: darkgreen;}.second-answer {color: darkblue;}.your-first-guess {color: darkgreen;}.your-second-guess {color: darkblue;}"))
+      tags$style(HTML(".real-probability {color: red;}.your-guess {color: blue;}"))
     ),
 
     # Page 1: Introduction
     conditionalPanel(
-      condition = "!input.page2_button",
+      condition = "!input.bday_page2_button",
       div(
         id = "page1",
-        style = "position: absolute; top: 50%; left: 53%;
-                 transform: translate(-50%, -50%); text-align: left;",
-        h3("Imagine you are at a Party"),
-        tags$img(
-          style = "height:288px; width:512px; scrolling=yes",
-          src = "party.jpg"
-        ),
-        h3("What do you think is the probability that one of the 70
-           people present shares the same day of birth as you in a year?"),
+        style = "position: absolute; top: 25%; left: 50%; transform: translate(-50%, -50%); text-align: left;",
+        h3("What do you think is the probability that two randomly selected people out of 70 in a room have the same day of birth in a year?"),
         div(
-          numericInput(ns("first_guess_input"),
-            "take your guess in percent",
-            value = 0, min = 0, max = 100
-          ),
+          numericInput(ns("guess_input"), "Take your guess in percent", value = 0, min = 0, max = 100),
           HTML("%"),
-        ),
-        h3("Time flies by 30 people have left, 40 still remain "),
-        br(),
-        h3("What do you think is the probability that two or more
-           people of 40 share the same day of birth in a year?"),
-        div(
-          numericInput(ns("second_guess_input"),
-            "take your guess in percent",
-            value = 0, min = 0, max = 100
-          ),
-          HTML("%"),
-          actionButton("page2_button", "Take Guess", )
+          actionButton("bday_page2_button", "Take Guess")
         )
       )
     ),
 
     # Page 2
     conditionalPanel(
-      condition = "input.page2_button",
+      condition = "input.bday_page2_button",
       div(
         id = "page2",
         sidebarLayout(
@@ -54,21 +33,18 @@ birthdayUI <- function(id) {
             sliderInput(ns("c"), "Number of categories:", min = 1, max = 365, value = 365),
             selectInput(
               ns("category_selector"),
-              label = "Example category: ",
+              label = "Example category:",
               choices = c("birthday in a year", "favourite planet of our solarsystem", "favorite element of the periodic table"),
               selected = "birthday in a year"
             ),
-            br()
+            br(),
+            downloadButton(ns("download_plot"), "Download Plot")
           ),
           mainPanel(
             tagList(
               plotOutput(ns("probability_plot")),
-              textOutput(ns("first_question_text")),
-              tags$span(textOutput(ns("first_answer_text")), class = "first-answer"),
-              tags$span(textOutput(ns("first_guess_text")), class = "your-first-guess"),
-              textOutput(ns("second_question_text")),
-              tags$span(textOutput(ns("second_answer_text")), class = "second-answer"),
-              tags$span(textOutput(ns("second_guess_text")), class = "your-second-guess"),
+              tags$span(textOutput(ns("probability_text")), class = "real-probability"),
+              tags$span(textOutput(ns("guess_text")), class = "your-guess"),
               br(),
               HTML("<h4>History</h4>"),
               textOutput(ns("text_description"))
